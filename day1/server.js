249a2246a8db5e2f2ddfabac09129dd3ce9e140c
Jhen Yu Huang <stone123p@yahoo.com.tw>
2015-07-07 04:11:57 +0000

    modified server.js

--- day1/server.js
+++ day1/server.js
@@ -5,10 +5,11 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.listen(port);
 
 app.get('/', function(req, res){
   res.send('江西來的老師：馬芙丸');
 });
+
+app.listen(port);
 
 console.log('Running server on port: ' + port);
