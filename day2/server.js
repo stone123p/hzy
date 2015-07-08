fb51624975a38a65b48d6371e0880c550fa19b21
Chi-Ren Hwang <chiren.hwang@gmail.com>
2015-07-08 10:34:14 +0800

    Add day2 initial files

---
+++ day2/server.js
@@ -0,0 +1,19 @@
+// 涵括 express 函式庫
+var express=require('express');
+
+//var path = require('path');
+
+// 將express模組指定到 app 變數
+var app=express();
+
+// 指定網站伺服器的埠號 3000
+var port = process.env.PORT || 3000;
+
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
+
+// 網站伺服器啟動，並接聽3000埠號
+app.listen(port);
+
+// 在 console 列印伺服器啟動的訊息。
+console.log('Running server on port:' + port);
