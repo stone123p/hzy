888cecc3bf7cca862813ff6c13af722cb5ed664f
Chi-Ren Hwang <chiren.hwang@gmail.com>
2015-07-10 16:05:25 +0800

    Add client and server codes

--- day4/client/app.js
+++ day4/client/app.js
@@ -1,7 +1,6 @@
 var React = require('react');
 var Images = require('./images.js');
-var imgs = ['01.jpg', 'minions.jpg', '03.jpg', '02.jpg'];
 React.render(
-    <Images imgs={imgs} dir='./img/' />,
+    <Images json='/cars' />,
     document.getElementById('app')
 );
