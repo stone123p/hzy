405482847d6a30527dcfe2fa2d72927fa0aaa961
Chi-Ren Hwang <chiren.hwang@gmail.com>
2015-07-14 20:38:09 +0800

    Remove unnecessary files

--- reflux-learn/client/app.js
+++ reflux-learn/client/app.js
@@ -1,7 +1,5 @@
 var React = require('react');
-//var Board = require('./board.js');
-//var Board = require('./board.state.js');  
-var Board = require('./board.add-and-search.js');//<--- 改為有新增與查詢的board.js
+var Board = require('./board.js');
 React.render(
   <Board />,
   document.getElementById('app')
